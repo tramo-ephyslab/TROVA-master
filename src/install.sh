299ee3be684184e400b7457f2e5218a7161b0e02
jcfernandez1993 <tramo-ephyslab>
2023-11-17 17:14:10 +0100

    old version removed

--- src/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-############################################################
-# Install fortran functions to use in python
-############################################################
-f2py -c -m functions functions.f90 
