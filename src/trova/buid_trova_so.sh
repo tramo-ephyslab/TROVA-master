af1ec25922dc16335601f1f15d0f8ffa85048444
tramo-ephyslab <jose.carlos.fernandez.alvarez@uvigo.es>
2023-11-20 12:46:25 +0100

    Modified

--- src/trova/buid_trova_so.sh
+++ src/trova/buid_trova_so.sh
@@ -4,13 +4,18 @@
 # Install fortran functions to use in python
 ############################################################
 
-FILE=`ls -a *.so`
+FILE=`ls -a *.so` 2>/dev/null
 echo $FILE
 
-if [ -f $FILE ]
+if [ -f "$FILE" ]
 then
+    echo "There are compiled fortran functions"
     rm *.so
     f2py -c --f90flags=-Wno-tabs -m functions functions.f90
 else
+    echo "Compiled fortran functions do not exist"
+    echo "------------------------------------"
+    echo "!!!!!!!Compiling functions...."
+    echo "------------------------------------"
     f2py -c --f90flags=-Wno-tabs -m functions functions.f90
 fi
